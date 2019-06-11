7e5c195468cc3a6d8c591995b92fef036e4ece38
Duane Stafford <duanestafford@Duanes-MBP.fios-router.home>
2019-06-10 23:49:45 -0400

    add images and game build

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,47 @@
 //Global variables
 var wins = 0;
 var losses = 0;
-var totalScore = "";
-var crystalOne = "";
-var crystalTwo = "";
-var crystalThree = "";
-var crystalFour = "";
-
+var totalScore = 0;
+var randomNumber;
+var randomCrystalOne;
+var randomCrystalTwo;
+var randomCrystalThree;
+var randomCrystalFour;
 
 //Random Number Variables
-var randomNumber = Math.floor((Math.random() * 102) + 19);
-var randomCrystalOne = Math.floor((Math.random() * 11) + 1);
-var randomCrystalTwo = Math.floor((Math.random() * 11) + 1);
-var randomCrystalThree = Math.floor((Math.random() * 11) + 1);
-var randomCrystalFour = Math.floor((Math.random() * 11) + 1);
+randomNumber = Math.floor((Math.random() * 102) + 19);
+randomCrystalOne = Math.floor((Math.random() * 11) + 1);
+randomCrystalTwo = Math.floor((Math.random() * 11) + 1);
+randomCrystalThree = Math.floor((Math.random() * 11) + 1);
+randomCrystalFour = Math.floor((Math.random() * 11) + 1);
 
 //Random Number Generator
 $(document).ready(function () {
 
     $("#random").html(randomNumber);
-    $("#fieldOne").html(randomCrystalOne);
-    $("#fieldTwo").html(randomCrystalTwo);
-    $("#fieldThree").html(randomCrystalThree);
-    $("#fieldFour").html(randomCrystalFour);
+    $("#fieldOne").attr("crystalValue", randomCrystalOne);
+    $("#fieldTwo").attr("crystalValue", randomCrystalTwo);
+    $("#fieldThree").attr("crystalValue", randomCrystalThree);
+    $("#fieldFour").attr("crystalValue", randomCrystalFour);
 
 });
+function resetGame() {
+    totalScore = 0;
+    $("#totalScore").text(totalScore);
+    randomNumber = Math.floor((Math.random() * 102) + 19);
+    $("#random").html(randomNumber);
+    randomCrystalOne = Math.floor((Math.random() * 11) + 1);
+    randomCrystalTwo = Math.floor((Math.random() * 11) + 1);
+    randomCrystalThree = Math.floor((Math.random() * 11) + 1);
+    randomCrystalFour = Math.floor((Math.random() * 11) + 1);
+    $("#fieldOne").attr("crystalValue", randomCrystalOne);
+    $("#fieldTwo").attr("crystalValue", randomCrystalTwo);
+    $("#fieldThree").attr("crystalValue", randomCrystalThree);
+    $("#fieldFour").attr("crystalValue", randomCrystalFour);
+    
+
+};
+
 //***Psuedo Code***
 //Hide randomCrystalX numbers
 
@@ -42,11 +59,34 @@ $(document).ready(function () {
 
 //Reset game by choosing a new randomNumber, randomCrystalOne, randomCrystalTwo, randomCrystalThree and randomCrystalFour
 
-var crystalValue = 0;
 
-function getRandomNumber() {
+$(".crystal").click(function () {
+    console.log();
+    var hiddenValue = Number($(this).attr("crystalValue"));
+    console.log(hiddenValue);
+    totalScore = totalScore + hiddenValue;
+    //Compare totalScore with randomNumber
+    if (totalScore < randomNumber) {
+        //Display totalScore
+        $("#totalScore").text(totalScore);
 
-};
+
+    } else if (totalScore > randomNumber) {
+        losses = losses + 1;//losses++
+        $("#losses").text(losses);
+        resetGame();
+        console.log("newRandomNumber", randomNumber)
+
+    }
+    else if (totalScore === randomNumber) {
+        wins = wins + 1;//wins++
+        $("#wins").text(wins);
+        console.log("win");
+        resetGame();
+        console.log("newRandomNumber", randomNumber)
+    }
+
+})
 
 
 
